09afb2c74ff62bfb67665e19f95862365149d16c
Matt Poegel <matt.poegel@gmail.com>
2024-09-15 18:00:50 +0000

    toggle guests display

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,25 @@
-console.log("hello");
+// let guestLevels = document.getElementsByClassName("guest-level");
+// for (let i = 0; i < guestLevels.length; i++) {
+//     guestLevels[i].addEventListener("mouseDown", function (e) {
+//         console.log(e);
+//     });
+// }
+
+function expandGuests(event) {
+    // console.log(event);
+    if (event.className === "guest-level") {
+        // expand
+        event.className = "guest-level-expanded";
+        for (let i = 0; i < event.children.length; i++) {
+            event.children[i].className = "guest-expanded";
+            event.children[i].children[1].className = "guest-name-expanded";
+        }
+    } else {
+        // collapse
+        event.className = "guest-level";
+        for (let i = 0; i < event.children.length; i++) {
+            event.children[i].className = "guest";
+            event.children[i].children[1].className = "guest-name";
+        }
+    }
+}
